c2edacc1423148c07cca7f2a70c1ae84a9a1f667
Brett Kuhlman <brett.w.kuhlman@gmail.com>
2020-06-23 11:58:27 -0400

    format

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "azurerm_app_service" "challenge1" {
 
   site_config {
     dotnet_framework_version = "v4.0"
-    scm_type = "GitHub"
+    scm_type                 = "GitHub"
   }
 
   app_settings = {
